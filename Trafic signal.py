0305024b8263dc3f0e25a9613da8129b4ab45bff
pranalim24 <mohitepranali05@gmail.com>
2026-07-27 14:13:13 +0530

    Add files via upload

---
+++ Trafic signal.py
@@ -0,0 +1,10 @@
+print("***Trafic Signal Rule***")
+
+
+signal=input("Enter the signal colour:")
+if signal == "red":
+    print("Action:Stop")
+elif signal == "yellow" :
+    print("Action:Wait")
+elif signal == "green" :
+ print("Action:Go")
