51d644d5d0ff53660b773a919c16ebb0e98bcc41
pranalim24 <mohitepranali05@gmail.com>
2026-07-28 12:42:27 +0530

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,9 @@
+cousrename=input("Enter Your Cousre Name:")
+prn=input("Enter Your PRN:")
+
+print("***Student Details***")
+print(input("Enter Your Name:"))
+print(input("Enter Your Roll No:"))
+print(input("Enter Your College Name:"))
+print(input("Enter Your Address:"))
+print(input("Enter Your ABC ID:"))
