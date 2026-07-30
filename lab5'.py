1a0c3d8b0edb5a817f710724fdfba2865c3ed69b
pranalim24 <mohitepranali05@gmail.com>
2026-07-30 01:27:57 -0700

    Add files via upload

--- lab5'.py
+++ lab5'.py
@@ -1,7 +1,11 @@
-for i in range(1,6):
+'''for i in range(1,6):
     for j in range(i):
         print(j+1,end="")
-    print()
+    print()'''
 
-      
-       
+           
+for i in range(6,1,-1):
+    for j in range(i-1):
+        print(j+1,end="")
+    print()
+  
