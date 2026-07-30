262ec51ab97df0a5c955b0a4fec894d4f95d65ce
pranalim24 <mohitepranali05@gmail.com>
2026-07-30 00:52:57 -0700

    Add files via upload

--- lab5.py
+++ lab5.py
@@ -1,4 +1,9 @@
-for i in range(1,6): #for outer loop
-     for j in range(i):   #for inner loop
+#for i in range(1,6): #for outer loop
+     #for j in range(i):   #for inner loop
+        #print("*",end="")
+     #print()
+
+for i in range(6,1,-1):
+    for j in range(i):
         print("*",end="")
-     print()
+    print()
